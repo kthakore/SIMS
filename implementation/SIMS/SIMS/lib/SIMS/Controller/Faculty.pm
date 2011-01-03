9200b8f360c3fd90a9735ad6740dba4433289aac
kthakore <thakore.kartik@gmail.com>
2011-01-03 11:38:57 -0500

    Trying to do a search student on DB

--- implementation/SIMS/SIMS/lib/SIMS/Controller/Faculty.pm
+++ implementation/SIMS/SIMS/lib/SIMS/Controller/Faculty.pm
@@ -25,11 +25,31 @@ sub base :Chained('/') PathPart('faculty') CaptureArgs(0) {
 	$c->response->redirect($c->uri_for('/unauthorized')) unless( grep /(g_admin|g_exec|adv_com|fac)/, @roles );
 }
 
+sub index :Chained('base') PathPart('') Args(0) {
+    my ( $self, $c ) = @_;
+}
+
+
 
 sub search_student :Chained('base') PathPart('search_student') Args(0) {
     my ( $self, $c ) = @_;
 
-    $c->response->body('Search for student .');
+	my $search_text = $c->req->param('search_text');
+
+	$c->log->debug("**Searching Student on $search_text");
+	my @found = $c->model('DB::Student')->search_like( 
+	
+		name => $search_text, type => $search_text,
+		address => $search_text, address2 => $search_text,
+		city => $search_text, province => $search_text,
+		postalcode => $search_text, phone => $search_text,
+		phone => $search_text, email => $search_text, location => $search_text
+
+	 );
+
+	$c->flash( search_result => \@found );
+	$c->res->redirect( $c->uri_for( $self->action_for('index') ) );
+
 }
 
 

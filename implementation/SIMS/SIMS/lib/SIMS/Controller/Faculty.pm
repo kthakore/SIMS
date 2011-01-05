c19b6de96e8f3fb769f6f7336baca0cb67c327a0
kthakore <thakore.kartik@gmail.com>
2011-01-05 10:18:56 -0500

    Elements of Dashboard

--- implementation/SIMS/SIMS/lib/SIMS/Controller/Faculty.pm
+++ implementation/SIMS/SIMS/lib/SIMS/Controller/Faculty.pm
@@ -26,27 +26,58 @@ sub base :Chained('/') PathPart('faculty') CaptureArgs(0) {
 }
 
 sub index :Chained('base') PathPart('') Args(0) {
-    my ( $self, $c ) = @_;
-
+	my ( $self, $c ) = @_;
+	my @found = $c->model('DB::Student')->all();
+	$c->stash( search_result => \@found );
 }
 
 
 
 sub search_student :Chained('base') PathPart('search_student') Args(0) {
-    my ( $self, $c ) = @_;
+	my ( $self, $c ) = @_;
 
 	my $search_text = $c->req->param('search_text');
 
-	$c->log->debug("**Searching Student on $search_text");
-	my @found = $c->model('DB::Student')->search({ name => { like => '%' . $search_text . '%' }});
+	my @found;
+
+	if( $search_text ) 
+	{
+
+		$c->log->debug("**Searching Student on $search_text");
+		my $cols_ref = $c->model('DB::Student')->result_source()->_columns();
+		my @cols = keys %$cols_ref;
+	
+		my @search_set = ();
+
+		foreach( @cols )
+		{
+			unless($_ eq 'id')
+			{
+				push (@search_set,  ($_=>{ like => '%' . $search_text . '%' } ));
+			}
+		}	
+
+		@found = $c->model('DB::Student')->search(
+			\@search_set
+		);
+
+	}
+	elsif( !exists($c->stash->{search_result} ) )
+	{
+		@found = $c->model('DB::Student')->all();
+
+	}
+
 
 	if($#found > -1)
 	{
-	$c->stash( search_result => \@found );
+
+		$c->flash( search_message => "");
+		$c->stash( search_result => \@found );
 	}
 	else
 	{
-	$c->flash( search_message => "No students found ");
+		$c->flash( search_message => "No students found ");
 	} 
 	$c->stash( template => 'faculty/index.tt' );
 
@@ -56,32 +87,32 @@ sub search_student :Chained('base') PathPart('search_student') Args(0) {
 sub view_student :Chained('base') PathPath('view_student') Args(1) {
 	my ($self, $c, $id ) = @_;
 
-	 $c->stash( template => 'student/index.tt',
-				student =>  $c->model('DB::Student')->find($id) );
+	$c->stash( template => 'student/index.tt',
+			student =>  $c->model('DB::Student')->find($id) );
 }
 
 sub edit_student :Chained('base') PathPath('edit_student') Args(1) {
 	my ($self, $c, $id ) = @_;
 
-	 $c->stash(  edit_student_url =>  $c->uri_for('edit_student'), 
-	 			  student =>  $c->model('DB::Student')->find($id), 
-	 			template => 'student/edit.tt' );
+	$c->stash(  edit_student_url =>  $c->uri_for('edit_student'), 
+			student =>  $c->model('DB::Student')->find($id), 
+			template => 'student/edit.tt' );
 	if( $c->req->param('submit') )
 	{
 		$c->stash->{student}->update(
-		{
-		name => $c->req->param('name'),	
-		type => $c->req->param('type'),
-		address => $c->req->param('address'),
-		address2 => $c->req->param('address2'),
-		city => $c->req->param('city'),
-		province => $c->req->param('province'),
-		postalcode => $c->req->param('postalcode'),
-		phone => $c->req->param('phone'),
-		location => $c->req->param('location'),
-		}
-		);	
-	
+				{
+				name => $c->req->param('name'),	
+				type => $c->req->param('type'),
+				address => $c->req->param('address'),
+				address2 => $c->req->param('address2'),
+				city => $c->req->param('city'),
+				province => $c->req->param('province'),
+				postalcode => $c->req->param('postalcode'),
+				phone => $c->req->param('phone'),
+				location => $c->req->param('location'),
+				}
+				);	
+
 		$c->stash(message => "Updated user!" );
 	}
 
@@ -89,9 +120,9 @@ sub edit_student :Chained('base') PathPath('edit_student') Args(1) {
 
 
 sub view_report :Chained('base') PathPart('view_report') Args(0) {
-    my ( $self, $c ) = @_;
+	my ( $self, $c ) = @_;
 
-    $c->response->body('View Reports');
+	$c->response->body('View Reports');
 }
 
 
@@ -101,11 +132,10 @@ Kartik Thakore,,,
 
 =head1 LICENSE
 
-This library is free software. You can redistribute it and/or modify
-it under the same terms as Perl itself.
+	   This library is free software. You can redistribute it and/or modify
+	   it under the same terms as Perl itself.
 
 =cut
 
 __PACKAGE__->meta->make_immutable;
-
 1;
